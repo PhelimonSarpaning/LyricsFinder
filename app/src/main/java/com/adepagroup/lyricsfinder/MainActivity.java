5568eb8273cfd68adfcf0c76b23b015174327178
Phelimon Sarpaning <sarpaningphelimon@gmail.com>
2019-03-26 15:34:14 -0500

    Initial commit

---
+++ app/src/main/java/com/adepagroup/lyricsfinder/MainActivity.java
@@ -0,0 +1,54 @@
+package com.adepagroup.lyricsfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+//import android.app.DownloadManager.Request;
+import com.android.volley.Request;
+import com.android.volley.toolbox.*;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+    EditText showSongName;
+    EditText showSongArtist;
+    String songName;
+    String songArtist;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void search(View view){
+
+
+        showSongName = (EditText) findViewById(R.id.songName);
+        songName = showSongName.getText().toString();
+
+        showSongArtist = (EditText) findViewById(R.id.songArtist);
+        songArtist = showSongArtist.getText().toString();
+
+        if(songName.equals("") || songArtist.equals("")){
+            Toast.makeText(this, "You can't leave either fields empty!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "Fasten up! Searching for lyrics.", Toast.LENGTH_SHORT).show();
+
+            lyricsPage(view);
+
+        }
+    }
+     //call scrolling page
+    public void lyricsPage (View view){
+        Intent intent = new Intent (this, ScrollingActivity.class);
+        startActivity(intent);
+    }
+
+
+}
---
+++ app/src/main/java/com/adepagroup/lyricsfinder/ScrollingActivity.java
@@ -0,0 +1,74 @@
+package com.adepagroup.lyricsfinder;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.net.Uri;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+       System.out.println("In scrolling Activity");
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+           //MainActivity.songName;
+            EditText songNameText = (EditText)findViewById(R.id.songName);
+           String songName = songNameText.getText().toString();
+            EditText songArtistText = (EditText)findViewById(R.id.songArtist);
+            String songArtist = songArtistText.getText().toString();
+            final TextView textView = (TextView) findViewById(R.id.songLyrics);
+// ...
+
+            // Instantiate the RequestQueue.
+            RequestQueue queue = Volley.newRequestQueue(this);
+            Uri.Builder builder = new Uri.Builder();
+            builder.scheme("https")
+                .authority("api.lyrics.ovh")
+                .appendPath("v1")
+                .appendPath(String.valueOf(songArtist))
+                    .appendPath(String.valueOf(songName));
+            String url = builder.build().toString();
+            //String url ="https://api.lyrics.ovh/v1/{songArtcoldplayist}/{songName}";
+
+            // Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            // Display the first 500 characters of the response string.
+                            textView.setText( response.toString());
+                            Log.d("response", response);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    textView.setText("That didn't work!");
+                }
+            });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+}
